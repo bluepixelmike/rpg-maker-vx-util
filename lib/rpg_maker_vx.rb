738482120fcf98c175bef37a72194c8573c265bc
Michael Miller <bluepixelmike@gmail.com>
2015-09-18 21:22:55 -0600

    Add a collection for actors

--- lib/rpg_maker_vx.rb
+++ lib/rpg_maker_vx.rb
@@ -5,5 +5,7 @@ end
 # Include extension with partial implementation of RGSS.
 require_relative 'rgss3'
 
+require_relative 'rpg_maker_vx/collection'
+require_relative 'rpg_maker_vx/actor_collection'
 require_relative 'rpg_maker_vx/database'
 require_relative 'rpg_maker_vx/project'
---
+++ lib/rpg_maker_vx/actor_collection.rb
@@ -0,0 +1,28 @@
+require 'rpg_maker_rgss3'
+require_relative 'collection'
+
+module RPGMakerVX
+
+  # Set of actor information.
+  class ActorCollection < Collection
+
+    # Creates an empty collection of actors.
+    def initialize
+      super(RPG::Actor)
+    end
+
+    # Loads a set of actors from a file.
+    # @param filename [String] Path to the actors file.
+    # @return [ActorsCollection]
+    def self.load(filename)
+      items = super.load_items(filename, RPG::Actor)
+      collection = ActorCollection.new
+      items.each do |item|
+        collection << item
+      end
+      collection
+    end
+
+  end
+
+end
