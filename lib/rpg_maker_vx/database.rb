4274c5951dbbb057302f3532c396b6a7c86cdfed
Michael Miller <bluepixelmike@gmail.com>
2015-09-18 21:45:39 -0600

    Add save and load for actors

--- lib/rpg_maker_vx/database.rb
+++ lib/rpg_maker_vx/database.rb
@@ -1,10 +1,18 @@
-require_relative 'database'
+require_relative 'actor_collection'
 
 module RPGMakerVX
 
   # Collects all of the data-driven objects into a single interface.
   class Database
 
+    # File extension for all data types.
+    DATA_FILE_EXTENSION = '.rvdata2'
+
+    # Name of the actors file.
+    ACTORS_FILE = 'Actors' + DATA_FILE_EXTENSION
+
+    # Provides access to actor information.
+    # @return [ActorCollection]
     attr_reader :actors
 
     attr_reader :classes
@@ -32,6 +40,40 @@ module RPGMakerVX
     attr_reader :system
 
     attr_reader :terms
+
+    # Creates an database with pre-populated resources.
+    # @param resources [Hash<Symbol => Object>]
+    def initialize(resources)
+      @actors = resources[:actors] || ActorCollection.new
+    end
+
+    # Loads the database components of a project.
+    # @param path [String] Path to the 'Data' directory in the project.
+    # @return [Database]
+    def self.load(path)
+      # Generate filepaths for each resource.
+      resource_paths = {
+          :actors => File.join(path, ACTORS_FILE)
+      }
+
+      # Load each resource.
+      resources = {
+          :actors => ActorCollection.load(resource_paths[:actors])
+      }
+
+      Database.new(resources)
+    end
+
+    # Saves all contents of the database to a project.
+    # @param path [String] Path to the 'Data' directory in the project.
+    # @return [void]
+    def save(path)
+      {
+          File.join(path, ACTORS_FILE) => @actors
+      }.each do |filepath, resource|
+        resource.save(filepath)
+      end
+    end
 
   end
 
