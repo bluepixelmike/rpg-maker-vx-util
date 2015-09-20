fc3a2663d534263fbbbc994bea2b069c58657fe2
Michael Miller <bluepixelmike@gmail.com>
2015-09-20 13:26:53 -0600

    Implement structured export

--- lib/rpg_maker_vx_util/script_converter.rb
+++ lib/rpg_maker_vx_util/script_converter.rb
@@ -1,9 +1,12 @@
+# encoding: UTF-8
+
 class String
   def snake_case
     self.gsub(/::/, '/').
         gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
         gsub(/([a-z\d])([A-Z])/,'\1_\2').
         tr('-', '_').
+        tr(' ', '_').
         downcase
   end
 end
@@ -20,7 +23,7 @@ module RPGMakerVXUtil
       # @param options [Hash] Additional export options.
       # @option options [Symbol] :layout How the scripts are stored in their exported format.
       #   The options are:
-      #   - +:dirs+ - Create a directory for each group of scripts and a single file for each script. A group is marked by having script name start with ?.
+      #   - +:dirs+ - Create a directory for each group of scripts and a single file for each script. A group is marked by having script name start with ▼ (\u25BC).
       #   - +:flat+ - Dump all scripts to a single directory. +dest+ represents the path to the directory.
       #   - +:file+ - Dump all scripts to a single file. +dest+ represents the path to the file.
       # @option options [Symbol] :line_endings Type of line ending to use.
@@ -56,7 +59,73 @@ module RPGMakerVXUtil
       # @return [void]
       # @note Empty scripts will be omitted.
       def export_to_structured_files(project, dest, options = { :line_endings => :crlf })
-        fail NotImplementedError
+        # Make destination directory if it doesn't already exist.
+        Dir.mkdir(dest) unless Dir.exist?(dest)
+
+        group        = ''
+        group_path   = ''
+        script_paths = []
+
+        line_ending = case(options[:line_endings])
+                        when :lf
+                          "\n"
+                        else # :crlf
+                          "\r\n"
+                      end
+
+        # Skip the no-name and empty scripts.
+        project.scripts.scripts.reject do |script|
+          script.name.empty? && script.contents.empty?
+        end.each do |script|
+          if m = script.name.match(/^\u25BC\s*(.*)/)
+            # This marks the beginning of a new group.
+            group      = m[1].snake_case
+            group_path = File.join(dest, group)
+            Dir.mkdir(group_path) unless Dir.exist?(group_path)
+          else
+            # This is just a script.
+            # Construct file name.
+            name = script.name.snake_case
+            file = name + '.rb'
+            path = File.join(group_path, file)
+
+            # Convert to the desired line-ending.
+            contents = script.contents
+            case(options[:line_endings])
+              when :lf
+                contents.gsub!("\r\n", "\n")
+              else # :crlf
+                contents.gsub!(/\r?\n/, "\r\n")
+            end
+
+            # Write the contents of the script to the file.
+            File.open(path, 'wb') do |f|
+              # Mark file as UTF-8.
+              f.write '# encoding: UTF-8'
+              f.write line_ending
+
+              # Dump the script to the file.
+              f.write contents
+            end
+
+            script_paths << "#{group}/#{name}"
+          end
+        end
+
+        # Create the "include-all" script.
+        top_name = File.basename(dest)
+        top_file = dest + '.rb'
+        File.open(top_file, 'wb') do |f|
+          # Mark file as UTF-8.
+          f.write '# encoding: UTF-8'
+          f.write line_ending
+
+          script_paths.each do |path|
+            rel_path = File.join(top_name, path)
+            f.write "require_relative '#{rel_path}'"
+            f.write line_ending
+          end
+        end
       end
 
       # Export from RPG Maker VX to files.
